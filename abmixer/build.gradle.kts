7266de06abcf2000d70d0dc8d1c6dc5922e4a870
Egor Kitselyuk <kitselyuk@mindbox.cloud>
2025-04-28 13:26:48 +0300

    Init commit

---
+++ abmixer/build.gradle.kts
@@ -0,0 +1,74 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    `maven-publish`
+}
+
+group = "cloud.mindbox"
+version = "1.0.0"
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            compileTaskProvider.configure {
+                compilerOptions {
+                    jvmTarget.set(JvmTarget.JVM_1_8)
+                }
+            }
+        }
+        publishLibraryVariants("release", "debug")
+    }
+
+    val xcFrameworkName = "AbMixer"
+    val xcf = XCFramework(xcFrameworkName)
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = xcFrameworkName
+            binaryOption("bundleId", "cloud.mindbox.mobile_sdk.${xcFrameworkName}")
+            xcf.add(this)
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            //put your multiplatform dependencies here
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+android {
+    namespace = "cloud.mindbox.mobile_sdk.abmixer"
+    compileSdk = 35
+    defaultConfig {
+        minSdk = 21
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+publishing {
+    publications.withType<MavenPublication> {
+        artifactId = "abmixer"
+    }
+}
